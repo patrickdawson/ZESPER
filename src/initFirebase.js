41469ec8356c5695c9fe59891a6e8a43b5e1fbc9
Patrick Dawson <patrick.dawson79@gmail.com>
2016-11-28 21:28:22 +0100

    Hosting firebase locally.

---
+++ src/initFirebase.js
@@ -0,0 +1,20 @@
+/**
+ * Created by patri_000 on 28.11.2016.
+ */
+
+// Initialize Firebase
+/*var config = {
+ apiKey: "AIzaSyClAjL9J028KiE7gawCacFLMBrXwWgZgo4",
+ authDomain: "zesper-3300e.firebaseapp.com",
+ databaseURL: "https://zesper-3300e.firebaseio.com",
+ storageBucket: "zesper-3300e.appspot.com",
+ messagingSenderId: "499963572180"
+ };*/
+var config = {
+  apiKey: "AIzaSyBzS9DpDF5_yktGkBfjZA1dzTuL1ukTBb8",
+  authDomain: "zesper-test.firebaseapp.com",
+  databaseURL: "https://zesper-test.firebaseio.com",
+  storageBucket: "zesper-test.appspot.com",
+  messagingSenderId: "1082093152617"
+};
+firebase.initializeApp(config);
